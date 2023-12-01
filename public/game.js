031af4c241d0002745bb8e0a0c76b318faf3b794
Albert the fluent <87913906+HarukaYamamoto0@users.noreply.github.com>
2023-12-01 10:58:29 -0300

    Update game.js

--- public/game.js
+++ public/game.js
@@ -4,7 +4,7 @@ export default function createGame(socket) {
   const state = {
     players: {},
     fruits: {},
-    maxFruits: 20,
+    maxFruits: 40,
     maximumNumberOfWinners: 3,
     screen: {
       width: 25,
